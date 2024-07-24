2bba15b81c54cb3ee8d8173c6ae3d711088628fd
PILIHU2022 <2812167783@qq.com>
2024-07-24 13:13:28 +0800

    Py:修改了备份Minecraft文件的Python代码，支持在12点后自动杀死当前代码的Python进程

--- Copy-Minecraft-Files-to-backup.py
+++ Copy-Minecraft-Files-to-backup.py
@@ -4,12 +4,13 @@ import time
 localhour = time.strftime('%H', time.localtime())
 location = '/home/PILIHU/.minecraft/versions/1.20.4/'
 backup_location = '~/Minecraft'
+current_pid = os.getpid()
 dir = os.listdir('/home/PILIHU/.minecraft/versions/1.20.4/saves/')
 while 2 == 1+1:
     if len(dir) == 0:
         break
-    if localhour == '12':
-        os._exit
+    if localhour >= '12':
+        os.system(f'kill {current_pid}')
     os.system(f"rsync -a -r --quiet --delete {location}* {backup_location}")
     os.system(f'cd {backup_location} && git add . && git commit -S -m "Normal backup" && git push main')
     time.sleep(1800)
