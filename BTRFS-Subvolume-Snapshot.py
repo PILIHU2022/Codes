645bde3d955e5113777be0d7d169f4e51bfd96ea
PILIHU2022 <2812167783@qq.com>
2024-07-29 12:51:58 +0800

    Py:Changed value name so that it's esay to read

--- BTRFS-Subvolume-Snapshot.py
+++ BTRFS-Subvolume-Snapshot.py
@@ -11,13 +11,13 @@ import time
 # print(x)
 # print(y)
 # print(z)
-location = '/Snapshots1'
+location = '/Snapshots'
 unfull_date = time.strftime('%Y-%m', time.localtime())
-today_dated = int(time.strftime('%d', time.localtime()))
-yesterday_date_of_day = today_dated - 1
-yesterday_full_date = f'{unfull_date}-{before_dated}'
+today_date_of_day = int(time.strftime('%d', time.localtime()))
+yesterday_date_of_day = today_date_of_day - 1
+yesterday_full_date = f'{unfull_date}-{yesterday_date_of_day}'
 today_date = time.strftime('%Y-%m-%d', time.localtime())
 # Btrfs delete yesterday's snapshot
-os.system(f'doas btrfs subvolume delete  / {location}/{yesterday_full_date')
+os.system(f'doas btrfs subvolume delete  / {location}/{yesterday_full_date}')
 # Btrfs create today's snapshot for root(/)
 os.system(f'doas btrfs subvolume snapshot / {location}/{today_date}')
