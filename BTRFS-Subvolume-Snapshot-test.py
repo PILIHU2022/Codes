1724145030efe3f8c206061adc44f9ed5e06a179
PILIHU2022 <2812167783@qq.com>
2024-08-04 12:02:49 +0800

    BTRFS:Use datetime to instead time,and changed get local time codes

--- BTRFS-Subvolume-Snapshot-test.py
+++ BTRFS-Subvolume-Snapshot-test.py
@@ -1,25 +1,18 @@
 '''Btrfs子卷快照'''
 import os
-import time
-# from datetime import datetime, timedelta
-# date = time.strftime('%Y-%m-%d',time.localtime())
-# before_date=date - timedelta(1)
-# print(before_date)
-# x = time.strftime('%Y-%m-%d', datetime.now()+timedelta(days=1))
-# z = datetime.now()+timedelta(days=1)
-# y = time.strftime("%Y-%m-%d")
-# print(x)
-# print(y)
-# print(z)
+# import time
+# from datetime import timedelta
+import datetime
 location = '/Snapshots'
-unfull_date = time.strftime('%Y-%m', time.localtime())
-today_dated = int(time.strftime('%d', time.localtime()))
-yesterday_date_of_day = today_dated - 1
-yesterday_full_date = f'{unfull_date}-{yesterday_date_of_day}'
-today_date = time.strftime('%Y-%m-%d', time.localtime())
-if yesterday_date_of_day = 0:
-    if 
+# unfull_date = time.strftime('%Y-%m', time.localtime())
+# today_dated = int(time.strftime('%d', time.localtime()))
+# yesterday_date_of_day = today_dated - 1
+# yesterday_full_date = f'{unfull_date}-{yesterday_date_of_day}'
+# today_date = time.strftime('%Y-%m-%d', time.localtime())
+today_date = datetime.date.today()
+oneday = datetime.timedelta(days=1)
+yesterday = today_date-oneday
 # Btrfs delete yesterday's snapshot
-os.system(f'doas btrfs subvolume delete  / {location}/{yesterday_full_date}')
+os.system(f'doas btrfs subvolume delete  / {location}/{yesterday}')
 # Btrfs create today's snapshot for root(/)
 os.system(f'doas btrfs subvolume snapshot / {location}/{today_date}')
