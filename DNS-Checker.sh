851c63631031af797008a0df074db3cace8d0ccb
PILIHU2022 <2812167783@qq.com>
2024-08-07 12:32:30 +0800

    DNS-Bash:DNS checker

--- DNS-Checker.sh
+++ DNS-Checker.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-for dns in $(cat ./dns.list); do
+for dns in $(cat ./DNS.list); do
     dig @${dns} github.com >> DNS-Checker.result
     echo $dns
     cat DNS-Checker.result | grep 'Query time' | sed 's/;; Query time://' | sed 's/^/Query time:/g'
