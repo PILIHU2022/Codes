d8022a5de6c17103202f4e34a4cea52b2e12f094
PILIHU2022 <2812167783@qq.com>
2024-08-09 21:22:35 +0800

    DNS-Py:Changed codes, add DNS-List.json

--- DNS-Checker.py
+++ DNS-Checker.py
@@ -2,9 +2,9 @@ import os
 print('Would you like to install a program to check which DNS works well(must,y or n)?')
 domain = input('Input a domain to check the DNS(e.g:github.com)')
 # os.system('sudo pacman -S bind')
-with open('./DNS.list') as file_object:
+with open('./DNS-List.json') as file_object:
     for dns in file_object:
         print(dns)
-        command = f'dig @{dns} {domain} | grep \'Query time\' | sed \'s/;;Query time://\' | sed \'s/^/ Query time:/g\' >> DNS-Checker.result'
-        print(command)
-        # os.system(command)
+        command = f'dig @{dns} {domain} | grep \'Query time\' | sed \'s/;;Query time://\' | sed \'s/^/ Query time:/g\' >> DNS-Checker.result | bash'
+        # print(command)
+        os.system(command)
