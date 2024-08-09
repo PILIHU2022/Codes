29395c76d01547c6370f26974486b46fea4bbddc
PILIHU2022 <2812167783@qq.com>
2024-08-09 11:12:59 +0800

    DNS-Bash:Changed code to display

--- DNS-Checker.sh
+++ DNS-Checker.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/bash
 for dns in $(cat ./DNS.list); do
+    rm ./DNS-Checker.result
+    touch ./DNS-Checker.result
+    chmod 777 ./DNS-Checker.result
     dig @${dns} github.com >> DNS-Checker.result
     echo $dns
-    cat DNS-Checker.result | grep 'Query time' | sed 's/;; Query time://' | sed 's/^/Query time:/g'
+    cat DNS-Checker.result | grep 'Query time' | sed 's/;; Query time://' | sed 's/^/ -&/g'
 done
