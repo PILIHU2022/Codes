5d74fb20a065ce6c5ebfe0cc10280d64f3947c6b
PILIHU2022 <2812167783@qq.com>
2024-08-24 16:17:11 +0800

    DNS-Py:DNS checker written in Python,wait for modify

--- subprogress-test.py
+++ subprogress-test.py
@@ -1,6 +1,6 @@
 # a script what uses dig to check DNS Query time and uses grep to filter result
 import subprocess,os
-os.system("touch DNS-Checker-py.result")
+# os.system("touch DNS-Checker-py.result")
 
 with open('./DNS.list') as dns_list:
     for dns in dns_list:
@@ -13,13 +13,13 @@ with open('./DNS.list') as dns_list:
         # grep_subprocess = subprocess.Popen(grep.split(), stdin=dig_subprocess.stdout, stdout=subprocess.PIPE)
         grep_subprocess = subprocess.Popen(
             ['grep', '''Query time:'''],
-            stdin=dig_subprocess.stdout, 
-            stdout=subprocess.PIPE, 
+            stdin=dig_subprocess.stdout,
+            stdout=subprocess.PIPE,
             text=True
         )
         grep_output,_ = grep_subprocess.communicate()
         print(grep_output)
-# wirte result into file
+# wirte result into file,incomplete
 with open('./DNS-Checker-py.result') as result:
     with open('./DNS.list') as dns_list_w:
         for dns_w in dns_list_w:
