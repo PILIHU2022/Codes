69e8253421cbb8bac7ab85776b909b2d2acede10
PILIHU2022 <2812167783@qq.com>
2024-09-21 16:39:13 +0800

    DNS-Py:DNS checker written in Python incomplete

--- subprogress-test-debug_version.py
+++ subprogress-test-debug_version.py
@@ -1,5 +1,6 @@
 # a script what uses dig to check DNS Query time and uses grep to filter result
-import subprocess, os
+import subprocess
+import os
 
 with open("./DNS.list") as dns_list:
     for dns in dns_list:
@@ -22,15 +23,10 @@ with open("./DNS.list") as dns_list:
             text=True,
         )
         sed_outputs, _ = sed_subprocess.communicate()
-        print(sed_outputs)
 
 # wirte result into file,incomplete
 with open("./DNS-Checker-py.result", "w") as result:
-    with open("./DNS.list") as dns_list_w:
         # To make sure that it can print one result for each DNS.
-        for dns, sed_output in zip(dns_list_w, sed_outputs):
-            print(dns)
-            print(sed_output)
         # sed_subprocess = subprocess.Popen(['sed','s/;; Query time: /Query time:/'], stdin=grep_subprocess.stdout)
         # output = dig_subprocess.communicate()[0]
         # print('grep','\';; Query time: \'')
